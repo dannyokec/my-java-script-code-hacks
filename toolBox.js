95a4a39afcb307f7c998ee1c981bfc54b927e8cd
DANIEL OKECHUKWU <69169530+dannyokec@users.noreply.github.com>
2023-03-03 13:59:16 +0100

    added objDyn

---
+++ toolBox.js
@@ -0,0 +1,20 @@
+class ToolBox{
+  
+  constructor()
+  {
+  }
+  
+   objDyn(obj,path) {
+      var schema = obj;  // a moving reference to internal objects within obj
+      var pList = path.split('.');
+      var len = pList.length;
+      for(var i = 0; i < len-1; i++) {
+          var elem = pList[i];
+          if( !schema[elem] ) schema[elem] = {}
+          schema = schema[elem];
+      }
+
+     return schema[pList[len-1]] 
+    }
+  
+}
